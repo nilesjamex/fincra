977885bdb569aed3fc68ba35f46b0dabbf77f3b9
egbaidomeh james <egbaidomehjamez@gmail.com>
2021-11-06 13:18:55 +0000

    layout fix

---
+++ .history/Components/pay_20211106130605.js
@@ -0,0 +1,287 @@
+            import React, {useState, useEffect, useContext} from 'react'
+            import { StepContext } from './../Contexts/StepContext';
+            import Axios from 'axios';
+            import Dropdown from 'react-dropdown';
+            import 'react-dropdown/style.css';
+            import styles from './pay.module.css'
+            const Pay = () => {
+                // state to control button disabling
+                const [enable, setEnable] = useState(true)
+                // state to control iban visibility
+                const [iban, setIban] = useState(false)
+                // state to show rate
+                const [receive, setReceive] = useState("")
+                // state to store all data
+                const [payData, setPayData] = useState({
+                    send: "",
+                    email: "",
+                    name: "",
+                    ibanno: "",
+                    swift: "",
+                    account: ""
+                })
+                const { send, email, name, ibanno, swift, account } = payData;
+
+                const onChange = (e) => {
+                    e.preventDefault();
+                    setPayData({...payData, [e.target.name]: e.target.value})
+                }
+
+                // context to move through stages
+            const {steps, front, back} = useContext(StepContext)
+
+
+                // onclick functions
+                const step2 = (e) => {
+                    e.preventDefault();
+                    front();
+                }
+
+                const submit2 = (e) => {
+                    e.preventDefault();
+                    front();
+                }
+                // api for exchange rates
+                const [info, setInfo] = useState([]);
+                const [from, setFrom] = useState("usd");
+                const [to, setTo] = useState("ngn");
+                const [options, setOptions] = useState([]);
+                const [showRates, setShowRates] = useState(false)
+                
+        
+        // Calling the api whenever the dependency changes
+        useEffect(() => {
+            Axios.get(
+        `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${from}.json`)
+        .then((res) => {
+            setInfo(res.data[from]);
+            })
+        }, [from]);
+        useEffect(() => {
+            setOptions(Object.keys(info));
+        }, [info])
+            
+        // Function to convert the currency
+        const compare = (e) => {
+                    e.preventDefault();
+                    const rate = info[to];
+                    const feeCalc = (1/100) * send;
+                    setFee(feeCalc);
+                    const sendRateCalc = (send - feeCalc);
+                    setSendRate(sendRateCalc);
+                    const receiveCalc = sendRateCalc * rate;
+                    setReceive(receiveCalc);
+                    setEnable(false);
+                    setShowRates(true);
+                    
+                }
+
+        // states for holding calculations of the exchange rates
+        const [fee, setFee] =useState("");
+        const [sendRate, setSendRate] = useState("")
+
+
+
+                switch (steps) {
+                    case 1:
+                        return (
+                            <div className={styles.move}>
+                            
+                    <div className={styles.container}>
+                    <div className={styles.paymentText}>
+                    <h2 className={styles.paymentTextMain}>One-time Payment</h2>
+                    <p className={styles.paymentTextSub}>Send money internationally</p>        
+                    </div>
+
+                    <form className={styles.form}  onSubmit={step2}> 
+                    <div style={{display: "flex",  marginBottom: "0.6rem"}}>
+                    <input className={styles.paymentinfo} 
+                    type="text" 
+                    name="send"
+                    value={send}
+                    onChange={onChange}
+                    required /> <Dropdown style={{height: "3.5rem", marginRight: "0.3rem"}} options={options} 
+                        onChange={(e) => { setFrom(e.value) }}
+            value={from} placeholder="From" />
+            </div>
+
+           {
+               showRates && <div className={styles.rateShow}>
+            <div className={styles.rateInfo}>
+            <h5>{fee} {from}</h5>
+            <p>Transfer fee</p>
+            </div>
+            <div className={styles.rateInfo}>
+            <h5>{sendRate} {from}</h5>
+            <p>Amount we’ll convert</p>
+            </div>
+            <div className={styles.rateInfo}>
+            <h5 style={{color: "#372271"}}>{info[to]} {to}</h5>
+            <p style={{color: "#372271"}}>Guaranteed rate (1hr)</p>
+            </div>
+            </div>
+           } 
+
+           <div style={{display: "flex",  marginBottom: "0.6rem"}}>
+                    <input className={styles.paymentinfo} type="text" 
+                    name="receive"
+                    value={receive}
+                    onChange={onChange}
+                    readOnly />
+                    <Dropdown options={options} 
+                        onChange={(e) => { setTo(e.value) }}
+            value={to} placeholder="to" />
+            </div>
+                    <button type="submit" className={styles.compare} onClick={compare}> Compare Rates</button>
+                    <button type="submit" className={styles.continue} disabled={enable}> Continue </button>
+                    </form>
+                    </div>
+                    </div>
+                );
+                case 2:
+                        return (
+                    <div className={styles.move}>
+                            
+                    <div className={styles.container}>
+                    <div className={styles.paymentText}>
+                    <h2 className={styles.paymentTextMain}>Your Recipient</h2>
+                    <p className={styles.paymentTextSub}>Who are you sending money to?</p>        
+                    </div>
+
+                    <form className={styles.form} onSubmit={submit2}>
+                    <div>
+                    <div className={styles.payinfo}>
+                    <label> Their email (optional)</label> 
+                    <input className={styles.paymentinfo} type="email"
+                    name="email"
+                    value={email}
+                    onChange={onChange}
+                    />
+                    </div>
+                    <div className={styles.payinfo}>
+                    <label> Full name of the account holder</label> 
+                    <input className={styles.paymentinfo} type="text"
+                    name="name"
+                    value={name}
+                    onChange={onChange}
+                    required/>
+                    </div>
+                    </div>
+
+                    <div>
+                    <div className={styles.paymentText}>
+                    <h2 className={styles.paymentTextMain}>Bank Details</h2>        
+                    </div>
+                    <div style={{display: "flex"}}>
+                    <h6 onClick={(e) => setIban(false)} style={{cursor: "pointer", margin: "0.6rem", 
+                    color: `${iban ? "#8B8F96" : "#4305EB"}`}}>Inside Europe</h6>
+                    <h6 onClick={(e) => setIban(true)} style={{cursor: "pointer", margin: "0.6rem", 
+                    color: `${iban ? "#4305EB" : "#8B8F96"}`}}>Outside Europe</h6>
+                    </div>
+                {
+                    !iban && <div>
+                    <div className={styles.payinfo}>
+                    <label> IBAN</label> 
+                    <input className={styles.paymentinfo} type="text"
+                    name="ibanno"
+                    value={ibanno}
+                    onChange={onChange}
+                    required
+                    placeholder="DE98370440018929829032"/>
+                    </div>
+                    </div>
+                }
+                {
+                    iban && <div>
+                    <div className={styles.payinfo}>
+                    <label> SWIFT / BIC code</label> 
+                    <input className={styles.paymentinfo} 
+                    type="text"
+                    name="swift"
+                    value={swift}
+                    onChange={onChange}
+                    required
+                    placeholder="BUKBGB22"/>
+                    </div>
+                    <div className={styles.payinfo}>
+                    <label> IBAN / Account Number</label> 
+                    <input className={styles.paymentinfo} 
+                    type="text"
+                    name="account"
+                    value={account}
+                    onChange={onChange}
+                    required
+                    placeholder="01234567891"/>
+                    </div>
+                    </div>
+                
+                }
+                </div>
+                    <button type="submit" className={styles.continue2}> Continue </button>
+                    </form>
+                    </div>
+                    </div>
+                );
+                case 3:
+                        return (
+                    <div className={styles.move}>
+                            
+                    <div className={styles.container}>
+                    <div className={styles.paymentText}>
+                    <h2 className={styles.paymentTextMain}>Review details of your transfer</h2>   
+                    <hr width="96%" style={{border: "1px solid #F2F2F2"}} />     
+                    </div>
+                    
+                    <div className={styles.rateShow}>
+            <div className={styles.rateInfo}>
+            <p>You Send</p>
+            <h5>{send} {from}</h5>
+            </div>
+            <div className={styles.rateInfo}>
+            <p>Total fees (included)</p>
+            <h5>{fee} {from}</h5>
+            </div>
+            <div className={styles.rateInfo}>
+            <p>Amount we’ll convert</p>
+            <h5>{sendRate} {from}</h5>
+            </div>
+            <div className={styles.rateInfo}>
+            <p>Guaranteed rate (1hr)</p>
+            <h5>{info[to]} {to}</h5>
+            </div>
+             <div className={styles.rateInfo}>
+            <p>{name} gets</p>
+            <h5>{receive} {to}</h5>
+            </div>
+             <div className={styles.rateInfo}>
+            <p>Guaranteed rate (1hr)</p>
+            <h5>{info[to]} {to}</h5>
+            </div>
+            <hr width="96%" style={{margin: "1.2rem 0 1.2rem 0.6rem", border: "1px solid #F2F2F2"}} />
+            </div>
+
+             <div className={styles.rateShow}>
+            <div className={styles.rateInfo}>
+            <p>Name</p>
+            <h5>{name}</h5>
+            </div>
+            <div className={styles.rateInfo}>
+            <p>Email Address</p>
+            <h5>{email}</h5>
+            </div>
+            <div className={styles.rateInfo}>
+            <p>IBAN / Account number</p>
+            <h5>{ibanno || account}</h5>
+            </div>
+            </div>
+             <button className={styles.continue3} disabled> Confirm and Continue </button>
+                   
+                    </div>
+                    </div>
+                );
+                }
+                
+            }
+
+            export default Pay;
+
---
+++ .history/Layout/header_20211103003109.js
@@ -0,0 +1,37 @@
+import React, {useState} from 'react'
+import styles from './header.module.css'
+import Image from 'next/image'
+
+const Header = () => {
+    const [value, setValue] = useState(0)
+
+    const dragHandler = (e) => {
+        setvalue(e.target.value)
+    }
+    return (
+        <nav className={styles.navbar}>
+        <div className={styles.fincra}>
+        <h3>
+        <Image src="../public/Images/logo.png" alt="logo" height="30px" width="20px" />
+        <Image src="/../public/Images/p.svg" alt="logo" height="17.77px" width="12.47px" />
+        <Image src="/../public/Images/a.svg" alt="logo" height="17.77px" width="10.26px"/>
+        <Image src="/../public/Images/y.svg" alt="logo" height="17.77px" width="12.91px" /></h3>
+        </div>
+        {/* <div className={styles.inputrange}>
+        <div style={{
+        backgroundColor: `linear-gradient(to right, #4305EB 64.83%, #636166 99.42%)`,
+        }} className="track">
+              <input min="0"
+              className={styles.input}
+              max="100"
+              onChange={dragHandler}
+              value={value}
+              type="range" />
+              <div className={styles.animateTrack}></div>
+              </div> 
+        </div> */}
+        </nav>
+    )
+}
+
+export default Header;
