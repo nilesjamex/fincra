d02355111cc4ed0d2c46747d262e533e90cf0165
egbaidomeh james <egbaidomehjamez@gmail.com>
2021-11-17 14:09:05 +0100

    removal of range

--- Layout/header.js
+++ Layout/header.js
@@ -12,10 +12,7 @@ const Header = () => {
         <nav className={styles.navbar}>
         <div className={styles.fincra}>
         <h3>
-        <Image src="/../public/Images/logo.png" alt="logo" height="30px" width="20px" />
-        <Image src="/../public/Images/p.svg" alt="logo" height="17.77px" width="12.47px" />
-        <Image src="/../public/Images/a.svg" alt="logo" height="17.77px" width="10.26px"/>
-        <Image src="/../public/Images/y.svg" alt="logo" height="17.77px" width="12.91px" /></h3>
+        PayPortal</h3>
         </div>
         <div className={styles.inputrange}>
         
